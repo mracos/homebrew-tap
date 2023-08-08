a5c3aca2453329c5e937d603fd0f6d73c81dacf4
marcos ferreira <marcosp4f@gmail.com>
2023-08-08 11:47:49 -0300

    Add calendr cask
    https://github.com/pakerwreah/Calendr

---
+++ Casks/calendr.rb
@@ -0,0 +1,11 @@
+cask "calendr" do
+  version "1.10.2"
+  sha256 "0ed284ed1b98680512e32258695514f8d6d069698b61e50cf23358e13c64edcd"
+
+  url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.app.zip"
+  appcast "https://github.com/pakerwreah/Calendr/releases.atom"
+  name "Calendr.app"
+  homepage "https://github.com/pakerwreah/Calendr"
+
+  app "Calendr.app"
+end
