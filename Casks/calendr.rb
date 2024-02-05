4310ef4ddb991de223ea1a97433bec119e7d864f
marcos ferreira <marcosp4f@gmail.com>
2024-02-04 23:12:49 -0300

    Fix Calendr appcast -> livecheck

--- Casks/calendr.rb
+++ Casks/calendr.rb
@@ -3,7 +3,13 @@ cask "calendr" do
   sha256 "0ed284ed1b98680512e32258695514f8d6d069698b61e50cf23358e13c64edcd"
 
   url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.app.zip"
-  appcast "https://github.com/pakerwreah/Calendr/releases.atom"
+  appcast
+
+  livecheck do
+    url "https://github.com/pakerwreah/Calendr/releases.atom"
+    strategy :sparkle, &:version
+  end
+
   name "Calendr.app"
   homepage "https://github.com/pakerwreah/Calendr"
 
