7f81f5a7f06cec8662fd8076cdd5edbc3ab4b8fe
marcos ferreira <marcosp4f@gmail.com>
2025-03-15 11:54:06 -0300

    wrap octave-app with `on_macos` block
    because of error
    
    ```
    Error: Cask 'octave-app' definition is invalid: invalid 'sha256' value: nil
    Error: Process completed with exit code 1.
    ```

--- Casks/octave-app.rb
+++ Casks/octave-app.rb
@@ -1,30 +1,32 @@
 cask "octave-app" do
-  version "9.2"
-  arch arm: "aarch64", intel: "x86-64"
+  on_macos do
+    version "9.2"
+    arch arm: "aarch64", intel: "x86-64"
 
-  url_suffix = on_arch_conditional arm: "", intel: "-intel"
-  sha256 arm: "94cf4ea473cc1ea5d0d6991bd40b13c6a0519841a42eec514b7b5200e865d799",
-         intel: "fe927dcc37c791b4eb6ce31a180500090031db25e1d98111fa32f637d2dc607b"
+    url_suffix = on_arch_conditional arm: "", intel: "-intel"
+    sha256 arm: "94cf4ea473cc1ea5d0d6991bd40b13c6a0519841a42eec514b7b5200e865d799",
+      intel: "fe927dcc37c791b4eb6ce31a180500090031db25e1d98111fa32f637d2dc607b"
 
-  url "https://github.com/octave-app/octave-app/releases/download/v#{version}/Octave-#{version}#{url_suffix}.dmg"
-  name "Octave.app"
-  homepage "https://octave-app.org"
+    url "https://github.com/octave-app/octave-app/releases/download/v#{version}/Octave-#{version}#{url_suffix}.dmg"
+    name "Octave.app"
+    homepage "https://octave-app.org"
 
-  livecheck do
-    url "https://github.com/octave-app/octave-app"
-    strategy :github_releases
-  end
+    livecheck do
+      url "https://github.com/octave-app/octave-app"
+      strategy :github_releases
+    end
 
-  app_name = "Octave-#{version}.app"
-  app app_name
-  binary "#{appdir}/#{app_name}/Contents/Resources/usr/bin/octave"
+    app_name = "Octave-#{version}.app"
+    app app_name
+    binary "#{appdir}/#{app_name}/Contents/Resources/usr/bin/octave"
 
-  # fix hardcoded perl for texinfo
-  # also see: https://github.com/octave-app/octave-app/issues/245
-  postflight do
-    Dir.glob("#{appdir}/#{app_name}/Contents/Resources/usr/opt/texinfo/bin/*").each do |file|
-      next unless File.file?(file)
-      inreplace file, "#!/usr/bin/perl5.30", "#!/usr/bin/perl"
+    # fix hardcoded perl for texinfo
+    # also see: https://github.com/octave-app/octave-app/issues/245
+    postflight do
+      Dir.glob("#{appdir}/#{app_name}/Contents/Resources/usr/opt/texinfo/bin/*").each do |file|
+        next unless File.file?(file)
+        inreplace file, "#!/usr/bin/perl5.30", "#!/usr/bin/perl"
+      end
     end
   end
 end
