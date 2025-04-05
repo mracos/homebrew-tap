94827015c7fb1f58a6bcfb019adf5fe52965df73
marcos ferreira <marcosp4f@gmail.com>
2025-04-05 12:15:33 -0300

    fixup! No check for linux sha256 (fix for bump CI)

--- Casks/octave-app.rb
+++ Casks/octave-app.rb
@@ -5,8 +5,8 @@ cask "octave-app" do
   url_suffix = on_arch_conditional arm: "", intel: "-intel"
   sha256 arm: "94cf4ea473cc1ea5d0d6991bd40b13c6a0519841a42eec514b7b5200e865d799",
          intel: "fe927dcc37c791b4eb6ce31a180500090031db25e1d98111fa32f637d2dc607b",
-         arm64_linux: :no_check,
-         x86_64_linux: :no_check
+         arm64_linux: "",
+         x86_64_linux: ""
 
   url "https://github.com/octave-app/octave-app/releases/download/v#{version}/Octave-#{version}#{url_suffix}.dmg"
   name "Octave.app"
