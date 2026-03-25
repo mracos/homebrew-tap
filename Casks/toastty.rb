a7fab2dc58660752f3835299eb06c5526de309a3
marcos ferreira <marcosp4f@gmail.com>
2026-03-25 02:04:48 -0300

    Add toastty

---
+++ Casks/toastty.rb
@@ -0,0 +1,16 @@
+cask "toastty" do
+  version "0.2.1"
+  sha256 "e106177a4396712428b89739f21b6b6b30f49693a7ca823bacc91dfac0964117"
+  url "https://github.com/figelwump/toastty/releases/download/v#{version}/Toastty-#{version}.dmg"
+
+  name "Toastty"
+  desc "Native macOS terminal multiplexer built on libghostty"
+  homepage "https://github.com/figelwump/toastty"
+
+  livecheck do
+    url "https://github.com/figelwump/toastty/releases/latest"
+    strategy :github_latest
+  end
+
+  app "Toastty.app"
+end
