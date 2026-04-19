052639b00b17d5a1a52846059d01898455dd6044
marcos ferreira <marcosp4f@gmail.com>
2026-04-18 20:54:01 -0600

    add abtop@0.2.12

---
+++ Formula/abtop.rb
@@ -0,0 +1,34 @@
+class Abtop < Formula
+  desc "AI agent monitor for your terminal"
+  homepage "https://github.com/graykode/abtop"
+  license "MIT"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/graykode/abtop/releases/download/v0.2.12/abtop-aarch64-apple-darwin.tar.xz"
+      sha256 "6788f44af440a3352f58b3007eb5d7e358c988a1e222daf7f871a4f0f9a4450a"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/graykode/abtop/releases/download/v0.2.12/abtop-x86_64-apple-darwin.tar.xz"
+      sha256 "52478fa683f6fea9fb995d2d355bd62ffa0e3316ff88a3990bd207c4693d2d2b"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/graykode/abtop/releases/download/v0.2.12/abtop-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "6f778ac62fc71edc3ddafe97479253e1cb1070cba0792a3e8f7fff24f4242fd5"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/graykode/abtop/releases/download/v0.2.12/abtop-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "da856d5ed21d02a91de581cb7bd56a7e594ba6d000099779e0e540cfb3f32225"
+    end
+  end
+
+  def install
+    bin.install "abtop"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/abtop --version")
+  end
+end
